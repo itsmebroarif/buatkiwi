7af5a084e1d931a8cc0c2d632647f1495bec4384
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-05-25 01:38:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if (url.searchParams.get('by') != null) {
 
 let footer = document.getElementById("credit");
 footer.innerHTML = sender;
-footer.href = " https://www.instagram.com/itsmebroarif.dev/ ";
+footer.href = " https://www.instagram.com/itsmebroarif/ ";
 
 document.querySelector(".tombol").addEventListener('click', function () {
   Swal.fire("Hi Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
