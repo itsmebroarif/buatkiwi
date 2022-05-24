dbee99da8eb588c02935f525219120b45ff676ea
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-05-25 01:39:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Arif";
+  sender = "Kaka ?";
 }
 
 let footer = document.getElementById("credit");
